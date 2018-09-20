d5ebedf5ac99fc14b3ae4a818980cbdca3d0099d
Matteo-Tarda <t_matteo@ymail.com>
2018-09-20 19:44:01 +0200

    Finished assignment.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5-robot
-P5 robot, sample sketch
+# HAL 9000
+I am a HAL 9000 computer. I became operational at the H.A.L. plant in Urbana, Illinois on the 12th of January 1992.
--- sketch.js
+++ sketch.js
@@ -1,23 +1,88 @@
-function setup() {
-	// define artboard size
-	createCanvas(500,500);
-	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+'use strict'
+function setup() { // eslint-disable-line
+    // define artboard size
+    createCanvas(500, 500);
+    // Set here the values that are not changing all over the sketch
 }
 
-function draw() {
-	background(204);
+let floorH = 50,
+    nModuli = 10,
+    nColonne = 5,
+    c = 0,
+    t = 0,
+    mod = 1,
+    sm = [];
 
-	//Neck
-	line(250, 285, 250, 345);
+function draw() { // eslint-disable-line
+    background(30);
+    strokeWeight(3);
 
-	//Wheel
-	ellipse(250, 465, 70, 70);
 
-	//Body
-	rect(200, 325, 100, 140);
 
-	//Head
-	ellipse(250, 240, 90, 90);
+    //background modules
+    rectMode(CORNER)
+    stroke(60);
+    for (let i = 0; i < nColonne; i++) {
+        for (let j = 0; j < nModuli; j++) {
+            fill('#2b7faf');
+            rect(i * width / nColonne, j * (height - floorH) / nModuli, width / nColonne, (height - floorH) / nModuli);
+        }
+    }
+
+    //animate background modules
+    fill('#a3e9ff11');
+    if (c % 61 > 0) {
+        for (let i = 0; i < sm.length; i++) {
+            for (let j = 0; j < t; j++) {
+                rect(~~(sm[i] / nModuli) * width / nColonne, (sm[i] % nModuli) * (height - floorH) / nModuli, width / nColonne, (height - floorH) / nModuli);
+            }
+        }
+    } else {
+        for (sm = []; sm.length < 17;) {
+            let tmp = ~~(Math.random() * nModuli * nColonne);
+            if (!sm.includes(tmp))
+                sm.push(tmp);
+        }
+    }
+
+    //back plate
+    rectMode(CENTER);
+    fill('#0005');
+    noStroke();
+    rect(width/2, height/2, (width/6)+10, (height/2)+10);
+    fill(40);
+    stroke(60);
+    rect(width/2, height/2, width/6, height/2);
+    fill(100);
+    noStroke();
+    rect(width/2, height*11/16, (width/6)-3, (height/8)-3);
+
+
+    //Eye
+    fill(0);
+    stroke(60);
+    ellipse(width/2, height/2, 65);
+
+    noStroke();
+    fill('#fb5');
+    ellipse((width/2), (height/2), 6);
+    fill('#ff000020');
+    for(let i = 0; i < 5;i++){
+        ellipse(width/2, height/2, 15+i*10);
+    }
+    fill('#FFF8');
+    ellipse((width/2)+15, (height/2)-15, 7);
+
+
+    stroke(60);
+    fill('#ff000004');
+    for (let j = 0; j < t/4; j++) {
+        ellipse(width/2, height/2, 65);
+    }
+
+    c++;
+    if (c % 61 > 0) {
+        t += mod;
+        c % 61 > 30 ? mod = -1 : mod = 1;
+    }
 }
